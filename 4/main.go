fa9010a343947c0eee058bf12d7d0b600d797388
Viktor Edman <viktor@edman.se>
2024-12-04 22:13:47 +0100

    TODO: Figure out how to mark searched coordinates

--- 4/main.go
+++ 4/main.go
@@ -19,8 +19,8 @@ func findXmas(input string) int {
 	xmasesFound := 0
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[i]); j++ {
-			result := horizontalSearch(grid, i, j)
-			xmasesFound += result
+			xmasesFound += horizontalSearch(grid, i, j)
+			xmasesFound += verticalSearch(grid, i, j)
 		}
 	}
 	return xmasesFound
@@ -30,15 +30,32 @@ func horizontalSearch(grid [][]string, y, x int) int {
 	searchString := ""
 	xmasesFound := 0
 	for i := x - 4; i < x+4; i++ {
-		if i < 0 || i > len(grid)-1 {
+		if i < 0 || i > len(grid[y])-1 {
 			continue
 		}
 		searchString += grid[y][i]
-		grid[y][i] = "."
 	}
 	if strings.Contains(searchString, "XMAS") {
-		fmt.Println(searchString)
 		xmasesFound++
+	}
+	if strings.Contains(searchString, "SAMX") {
+		xmasesFound++
+	}
+	return xmasesFound
+}
+
+func verticalSearch(grid [][]string, y, x int) int {
+	searchString := ""
+	xmasesFound := 0
+	for i := y - 4; i < y+4; i++ {
+		if i < 0 || i > len(grid)-1 {
+			continue
+		}
+		searchString += grid[i][x]
+	}
+	if strings.Contains(searchString, "XMAS") {
+		xmasesFound++
+
 	}
 	if strings.Contains(searchString, "SAMX") {
 		xmasesFound++
